2b137aeae4b7230eb332adefb2ee2bc16ed498df
teki.sei <teki.sei@uzabase.com>
2025-04-16 17:06:04 +0900

    macOS

--- src/download.sh
+++ src/download.sh
@@ -1,6 +1,8 @@
+#! /bin/bash
+
 mkdir -p data
 
-wget https://www.rondhuit.com/download/ldcc-20140209.tar.gz
+curl -O https://www.rondhuit.com/download/ldcc-20140209.tar.gz
 mv ./ldcc-20140209.tar.gz ./data/
 cd data
 tar -zxvf ./ldcc-20140209.tar.gz
